425b1dc8e7c1dcdfb5b63fad5095740d4e1c14cc
xushan <1361720492@qq.com>
2022-03-11 15:31:42 +0800

    'Cglib'

---
+++ src/com/designPatterns/代理模式/Cglib/HelloWorldCglib.java
@@ -0,0 +1,17 @@
+package com.designPatterns.代理模式.Cglib;
+
+/**
+ * @Author: xs
+ * @describe:
+ * @date 2022/3/9 11:10
+ */
+public class HelloWorldCglib {
+
+    public void morning(String name) {
+        System.out.println("Good morning, " + name);
+    }
+
+    public String handle(String str) {
+        return str + "Test";
+    }
+}
---
+++ src/com/designPatterns/代理模式/Cglib/ProxyFactory.java
@@ -0,0 +1,39 @@
+package com.designPatterns.代理模式.Cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * @Author: xs
+ * @describe: 代理工厂
+ * @date 2022/3/11 14:40
+ */
+public class ProxyFactory<T> implements MethodInterceptor {
+    T target;
+
+    public ProxyFactory(T target) {
+        this.target = target;
+    }
+
+    public T getTarget() {
+        //1.创建工具类
+        Enhancer enhancer = new Enhancer();
+        //2.设置父类
+        enhancer.setSuperclass(target.getClass());
+        //3.设置回调函数
+        enhancer.setCallback(this);
+        //4.创建子类对象(代理对象)
+        return (T) enhancer.create();
+    }
+
+    @Override
+    public Object intercept(Object args0, Method method, Object[] args1, MethodProxy args2) throws Throwable {
+        System.out.println("Cglib代理模式开始");
+        Object invoke = method.invoke(target, args1);
+        System.out.println("Cglib代理模式结束");
+        return invoke;
+    }
+}
